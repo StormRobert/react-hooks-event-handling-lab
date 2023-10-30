23d145d72cc0d481f531f43ca4723a9bcfa49e35
storm <stormarasa2@gmail.com>
2023-10-30 11:02:05 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React, {useState} from "react";
+
+function EyesOnMe(){
+const[focus, setFocus]= useState(true);
+function Focus(){
+    console.log("Good!")
+    setFocus(true);
+}
+
+function Blur(){
+    console.log("Hey! Eyes on me!")
+    setFocus(false)
+}
+
+    return(
+        <div>
+            <button onFocus={Focus} onBlur= {Blur}>
+            {focus ? console.log("Good!") : console.log("Hey! Eyes on me!")}
+            <p> Eyes on me</p>
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
+import React, {useState} from 'react'
 
 function Keypad (){
+    const [display, setDisplay] = useState('')
+
+    function setDisplayPassword(event){
+        console.log("Entering password...")
+        setDisplay(event.target.value)
+    }
+    
     return (
-        <div></div>
+        <div>
+            <input type= "password" value={display} onChange={setDisplayPassword} />
+        </div>
     )
 }
 
